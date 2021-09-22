42715fa0f9883414e02da97dd1223fff9a4db833
Marcio Felipe Queiroga <marcio.felipe@seap.ma.gov.br>
2021-09-22 18:44:34 -0300

    ClientRepository, ClientService e ClienteResource com CRUD completo

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,7 +1,9 @@
+--Inserir Categorias
 INSERT INTO tb_category (name, created_At) VALUES ('Livros', NOW());
 INSERT INTO tb_category (name, created_At) VALUES ('Eletrônicos', NOW());
 INSERT INTO tb_category (name, created_At) VALUES ('Computadores', NOW());
 
+--Inserir Produtos
 INSERT INTO tb_product (name, price, date, description, img_url) VALUES ('The Lord of the Rings', 90.5, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg');
 INSERT INTO tb_product (name, price, date, description, img_url) VALUES ('Smart TV', 2190.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg');
 INSERT INTO tb_product (name, price, date, description, img_url) VALUES ('Macbook Pro', 1250.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg');
@@ -28,6 +30,7 @@ INSERT INTO tb_product (name, price, date, description, img_url) VALUES ('PC Gam
 INSERT INTO tb_product (name, price, date, description, img_url) VALUES ('PC Gamer Boo', 2350.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/24-big.jpg');
 INSERT INTO tb_product (name, price, date, description, img_url) VALUES ('PC Gamer Foo', 4170.0, TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/25-big.jpg');
 
+--Associar Produtos as Categorias
 INSERT INTO tb_product_category (product_id, category_id) VALUES (1, 2);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 1);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 3);
@@ -54,3 +57,40 @@ INSERT INTO tb_product_category (product_id, category_id) VALUES (22, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (23, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (24, 3);
 INSERT INTO tb_product_category (product_id, category_id) VALUES (25, 3);
+
+--Inserir Clientes
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Anderson Moraes Barbosa', '57783145825',4500.0 ,TIMESTAMP WITH TIME ZONE '1991-07-14T10:00:00Z', 0 )
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Laise Oliveira Aparecida Viana', '02430750228', 5731.9, TIMESTAMP WITH TIME ZONE '1985-07-14T10:00:00Z', 2)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sandra Helena Vera Silva','02972335481',3260.0, TIMESTAMP WITH TIME ZONE '1999-03-12T17:50:30Z', 0)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Julio Levi Murilo Figueiredo','95845777437',3950.1, TIMESTAMP WITH TIME ZONE '1987-06-15T23:10:00Z', 1)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Enzo Eduardo Mário Dias','22325440362',7230.0, TIMESTAMP WITH TIME ZONE '1995-12-27T11:50:00Z', 0)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Nelson Filipe Anderson da Mata','08127860174',4551.7, TIMESTAMP WITH TIME ZONE '1991-03-13T15:07:00Z', 0)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bento Enrico Julio da Rocha','61149606703',5140.43, TIMESTAMP WITH TIME ZONE '1967-05-06T19:31:00Z', 3)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Filipe Victor Ruan da Mata','54697826205',2700.50, TIMESTAMP WITH TIME ZONE '1989-09-03T13:42:00Z', 0)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Jéssica Manuela Drumond','299930094730',9670.30, TIMESTAMP WITH TIME ZONE '1976-02-21T08:21:00Z', 2)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Clarice Heloisa Isabelly Jesus','06833770271',3108.0, TIMESTAMP WITH TIME ZONE '2006-10-19T03:53:00Z', 0)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Oliver Elias da Silva','71589263626',4810.0, TIMESTAMP WITH TIME ZONE '2001-01-15T12:10:00Z', 1)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sandra Andreia da Rosa','22577277113',4656.96, TIMESTAMP WITH TIME ZONE '1949-10-25T12:10:00Z', 6)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Olivia Carolina Dias','58988650042',4365.77, TIMESTAMP WITH TIME ZONE '1956-10-19T12:10:00Z', 4)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Levi Martin Caue Gomes','76962566298',1580.18, TIMESTAMP WITH TIME ZONE '1956-04-27T12:10:00Z', 1)
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Stefany Adriana dos Santos','90931119065',2652.76, TIMESTAMP WITH TIME ZONE '1942-07-04T12:10:00Z', 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
