8b4d6fcd17dcb25e6bc924694d064f7b924d95ad
Marcio Felipe Queiroga <marcio.felipe@seap.ma.gov.br>
2021-10-05 11:07:45 -0300

    Refactor ClientService pageable

--- backend/src/main/java/com/devsuperior/dscatalog/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ClientService.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,9 +25,9 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+	public Page<ClientDTO> findAllPaged(Pageable pageable) {
 
-		Page<Client> list = repository.findAll(pageRequest);
+		Page<Client> list = repository.findAll(pageable);
 
 		return list.map(x -> new ClientDTO(x));
 	}
