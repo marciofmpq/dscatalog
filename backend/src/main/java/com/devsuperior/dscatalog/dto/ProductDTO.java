40dc8d64620c2499d1387e7d4287994f9d38ff7e
Marcio Felipe Queiroga <marcio.felipe@seap.ma.gov.br>
2021-12-28 11:08:01 -0300

    Basic BeanValidation

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -7,6 +7,11 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 
@@ -15,10 +20,19 @@ public class ProductDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@Size(min = 5 , max = 60 , message = "Deve ter entre 5 e 60 Caracteres")
+	@NotBlank(message = "Campo Obrigatorio")
 	private String name;
+	
+	@NotBlank(message = "Campo Obrigatorio")
 	private String description;
+	
+	@Positive(message = "Preco deve ser valor positivo")
 	private Double price;
 	private String imgUrl;
+	
+	@PastOrPresent(message = "Data nao pode ser futura")
 	private Instant date;
 	
 	private List<CategoryDTO> categories = new ArrayList<>();
--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -5,6 +5,9 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.devsuperior.dscatalog.entities.User;
 
 public class UserDTO implements Serializable{
@@ -12,8 +15,12 @@ public class UserDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank(message = "Campo Obrigatorio")
 	private String firstName;
 	private String lastName;
+	
+	@Email(message = "Entre com um email valido")
 	private String email;
 	
 	Set<RoleDTO> roles = new HashSet<>();
